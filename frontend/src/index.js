9abe0a68399b1903036d24299c87fc89fbbb7171
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2024-01-19 13:41:12 +0530

    8 auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { SurveysContextProvider } from './context/SurveysContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { SurveysContextProvider } from "./context/SurveysContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SurveysContextProvider>
-    <App />
-    </SurveysContextProvider>
+    <AuthContextProvider>
+      <SurveysContextProvider>
+        <App />
+      </SurveysContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
-
-
